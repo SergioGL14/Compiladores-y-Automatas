421b5119a25bcb5634724f13b4e303054a360a4b
SergioGL14 <ga401129@uaeh.edu.mx>
2024-10-23 20:08:34 -0600

    3.2 Práctica. Analizar códigos de C++ con LEX
    3.2 Práctica. Analizar códigos de C++ con LEX

---
+++ SINTACTICO/Practica3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+//Prueba
+//Comentatio
+int main() {
+    std::string cadena;
+    bool tieneLetras = false, tieneDigitos = false;
+//Ingreso de cadena 
+    std::cout << "Ingresa la cadena: ";
+    std::getline(std::cin, cadena);
+
+    for (std::string::size_type i = 0; i < cadena.length(); ++i) {
+        char ch = cadena[i];
+        if (std::isalpha(ch)) {
+            tieneLetras = true;
+        } else if (std::isdigit(ch)) {
+            tieneDigitos = true;
+        }
+    }
+
+    if (tieneLetras && tieneDigitos) {
+        std::cout << "Compuesta\n";
+    } else if (tieneLetras) {
+        std::cout << "Palabra\n";
+    } else if (tieneDigitos) {
+        std::cout << "Numero entero\n";
+    } else {
+        std::cout << "La cadena no contiene letras ni números\n";
+    }
+
+    return 0;
+}
+
+
